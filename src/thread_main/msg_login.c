7c38d55a2b4e59bf8ba66389132be68af44efafd
AliyunESC <itsalbertzhang@gmail.com>
2022-05-23 20:51:24 +0800

    2205232050

--- src/thread_main/msg_login.c
+++ src/thread_main/msg_login.c
@@ -60,55 +60,30 @@ int msg_login(const char *cmd) {
     bzero(&recvbuf, sizeof(recvbuf));
     sendbuf.msgtype = MT_LOGIN;
 
-    int pwd_check = DISAPPROVE;
     char pwd_plaintext[64] = {0};
 
-    ret = sscanf(cmd, "%*s%s%s", sendbuf.username, pwd_plaintext);
-    if (ret > 0) {
-        sendbuf.username_len = strlen(sendbuf.username);
-        if (strlen(pwd_plaintext) >= 3 || strlen(pwd_plaintext) <= 30) {
-            pwd_check = APPROVE;
-        }
-    } else {
-        // 获取用户名
-        printf("[LOGIN] 请输入你的用户名: ");
-        fflush(stdout);
-        scanf("%s", sendbuf.username);
-        sendbuf.username_len = strlen(sendbuf.username);
+    if (sscanf(cmd, "%*s%s%s", sendbuf.username, pwd_plaintext) <= 0 || strlen(pwd_plaintext) < 3 || strlen(pwd_plaintext) > 30) {
+        logging(LOG_ERROR, "格式错误! 请输入 \"login 用户名 密码\" 以登录.");
+        return -1;
     }
 
-    while (DISAPPROVE == pwd_check) {
-        char *pwd_plaintext_p = getpass("[LOGIN] 请输入密码(无回显), 可输入 exit 退出:");
-        // 如果用户希望中断登录 (在 getpass 函数中无法通过 SIGINT 信号退出), 则跳出循环. 注意到此时会直接跳转到函数尾注册失败的情形.
-        if (0 == strcmp(pwd_plaintext_p, "exit")) {
-            break;
-        }
-        // 如果输入的密码不符合规范, 则重新进行循环.
-        if (strlen(pwd_plaintext_p) < 3 || strlen(pwd_plaintext_p) > 30) {
-            printf("[ERROR] 密码长度过短或过长, 请确保密码为 3 ~ 30 个字符.\n");
-            continue;
-        }
-        strcpy(pwd_plaintext, pwd_plaintext_p);
-        pwd_check = APPROVE;
-    }
+    sendbuf.username_len = strlen(sendbuf.username);
 
-    if (APPROVE == pwd_check) {
-        // 对密码进行 token2nd 异或
-        for (int i = 0; i < strlen(pwd_plaintext); i++) {
-            pwd_plaintext[i] = pwd_plaintext[i] ^ program_stat->token2nd[i];
-        }
-        // 对异或后的密码进行 rsa 加密
-        sendbuf.pwd_ciprsa_len = rsa_encrypt(pwd_plaintext, sendbuf.pwd_ciprsa, program_stat->serverpub_rsa, PUBKEY);
-        RET_CHECK_BLACKLIST(-1, sendbuf.pwd_ciprsa_len, "rsa_encrypt");
-        // 销毁密码明文, 确保安全
-        bzero(pwd_plaintext, sizeof(pwd_plaintext));
-        // 向服务端发送登录信息.
-        ret = msg_login_send(program_stat->connect_fd, &sendbuf);
-        RET_CHECK_BLACKLIST(-1, ret, "msg_login_send");
-        // 获取服务端的回复信息.
-        ret = msg_login_recv(program_stat->connect_fd, &recvbuf);
-        RET_CHECK_BLACKLIST(-1, ret, "msg_login_recv");
+    // 将密码与 token 第二部分异或
+    for (int i = 0; i < strlen(pwd_plaintext); i++) {
+        pwd_plaintext[i] = pwd_plaintext[i] ^ program_stat->token2nd[i];
     }
+    // 对异或后的密码进行 rsa 加密
+    sendbuf.pwd_ciprsa_len = rsa_encrypt(pwd_plaintext, sendbuf.pwd_ciprsa, program_stat->serverpub_rsa, PUBKEY);
+    RET_CHECK_BLACKLIST(-1, sendbuf.pwd_ciprsa_len, "rsa_encrypt");
+    // 销毁密码明文, 确保安全
+    bzero(pwd_plaintext, sizeof(pwd_plaintext));
+    // 向服务端发送登录信息
+    ret = msg_login_send(program_stat->connect_fd, &sendbuf);
+    RET_CHECK_BLACKLIST(-1, ret, "msg_login_send");
+    // 获取服务端的回复信息.
+    ret = msg_login_recv(program_stat->connect_fd, &recvbuf);
+    RET_CHECK_BLACKLIST(-1, ret, "msg_login_recv");
 
     if (APPROVE == recvbuf.approve) {
         logging(LOG_INFO, "登录成功.");
--- src/thread_main/msg_regist.c
+++ src/thread_main/msg_regist.c
@@ -58,83 +58,30 @@ int msg_regist(const char *cmd) {
     bzero(&recvbuf, sizeof(recvbuf));
     sendbuf.msgtype = MT_REGIST;
 
-    int pwd_check = DISAPPROVE;
     char pwd_plaintext[64] = {0};
 
-    ret = sscanf(cmd, "%*s%s%s", sendbuf.username, pwd_plaintext);
-    if (ret > 0) {
-        sendbuf.username_len = strlen(sendbuf.username);
-        if (strlen(pwd_plaintext) >= 3 || strlen(pwd_plaintext) <= 30) {
-            pwd_check = APPROVE;
-        }
-    } else {
-        // 核验用户名. 期间可能有与服务端的多次通信.
-        recvbuf.approve = DISAPPROVE;
-        while (DISAPPROVE == recvbuf.approve) {
-            // 获取用户希望注册的用户名
-            printf("[REGIST] 请输入想要注册的用户名:");
-            fflush(stdout);
-            scanf("%s", sendbuf.username);
-            sendbuf.username_len = strlen(sendbuf.username);
-            // 向服务端发送核验用户名的消息. 此时密码为空 (之前已经对 sendbuf 结构体进行过 bzero).
-            ret = msg_regist_send(program_stat->connect_fd, &sendbuf);
-            RET_CHECK_BLACKLIST(-1, ret, "msg_regist_send");
-            // 获取服务端的回复信息
-            ret = msg_regist_recv(program_stat->connect_fd, &recvbuf);
-            RET_CHECK_BLACKLIST(-1, ret, "msg_regist_recv");
-            if (DISAPPROVE == recvbuf.approve) { // 说明服务端拒绝了该用户名
-                printf("[REGIST] 无法使用该用户名. 请重试.\n");
-            }
-        }
+    if (sscanf(cmd, "%*s%s%s", sendbuf.username, pwd_plaintext) <= 0 || strlen(pwd_plaintext) < 3 || strlen(pwd_plaintext) > 30) {
+        logging(LOG_ERROR, "格式错误! 请输入 \"regist 用户名 密码\" 以注册.");
+        return -1;
     }
 
-    // 获取密码并进行正式注册
-    char *pwd_plaintext_p = NULL;
-    while (DISAPPROVE == pwd_check) {
-        // 第一次从用户输入中获取密码
-        pwd_plaintext_p = getpass("[REGIST] 请输入密码(无回显), 可输入 exit 退出:");
-        // 如果用户希望中断注册 (在 getpass 函数中无法通过 SIGINT 信号退出), 则跳出循环. 注意到此时会直接跳转到函数尾注册失败的情形.
-        if (0 == strcmp(pwd_plaintext_p, "exit")) {
-            break;
-        }
-        // 如果第一次输入的密码不符合规范, 则重新进行循环.
-        if (strlen(pwd_plaintext_p) < 3 || strlen(pwd_plaintext_p) > 30) {
-            printf("[ERROR]  密码长度过短或过长, 请确保密码为 3 ~ 30 个字符.\n");
-            continue;
-        }
-        // 将第一次输入的密码归档保存
-        strcpy(pwd_plaintext, pwd_plaintext_p);
-        // 第二次从用户输入中获取密码
-        pwd_plaintext_p = getpass("[REGIST] 请再次输入密码(无回显):");
-        // 如果两次输入的密码不一致, 则重新进行循环.
-        if (0 != strcmp(pwd_plaintext_p, pwd_plaintext)) {
-            printf("[ERROR]  两次输入的密码不一致. 请重新输入.\n");
-            continue;
-        }
-        // 如果一切步骤正常进行, 说明密码本地核验通过
-        pwd_check = APPROVE;
+    sendbuf.username_len = strlen(sendbuf.username);
+
+    // 对密码进行 token2nd 异或
+    for (int i = 0; i < strlen(pwd_plaintext); i++) {
+        pwd_plaintext[i] = pwd_plaintext[i] ^ program_stat->token2nd[i];
     }
-    // 在密码本地核验通过的情况下, 向服务端正式提交注册申请
-    if (APPROVE == pwd_check) {
-        // 对密码进行 token2nd 异或
-        for (int i = 0; i < strlen(pwd_plaintext); i++) {
-            pwd_plaintext[i] = pwd_plaintext[i] ^ program_stat->token2nd[i];
-        }
-        // 对异或后的密码进行 rsa 加密
-        sendbuf.pwd_ciprsa_len = rsa_encrypt(pwd_plaintext, sendbuf.pwd_ciprsa, program_stat->serverpub_rsa, PUBKEY);
-        RET_CHECK_BLACKLIST(-1, sendbuf.pwd_ciprsa_len, "rsa_encrypt");
-        // 销毁密码明文, 确保安全
-        bzero(pwd_plaintext, sizeof(pwd_plaintext));
-        pwd_plaintext_p = NULL;
-        // 向服务端发送正式的注册信息. 这条信息同时包括用户名和密码.
-        ret = msg_regist_send(program_stat->connect_fd, &sendbuf);
-        RET_CHECK_BLACKLIST(-1, ret, "msg_regist_send");
-        // 获取服务端的回复信息. 注意, 服务端在客户端的一个连接进行注册的整个过程中并非是阻塞等待的.
-        // 因此在极端情况下, 服务端仍旧可能不批准注册请求.
-        // 如: 在核验用户名通过后, 有其他连接抢先一步注册了该用户名.
-        ret = msg_regist_recv(program_stat->connect_fd, &recvbuf);
-        RET_CHECK_BLACKLIST(-1, ret, "msg_regist_recv");
-    }
+    // 对异或后的密码进行 rsa 加密
+    sendbuf.pwd_ciprsa_len = rsa_encrypt(pwd_plaintext, sendbuf.pwd_ciprsa, program_stat->serverpub_rsa, PUBKEY);
+    RET_CHECK_BLACKLIST(-1, sendbuf.pwd_ciprsa_len, "rsa_encrypt");
+    // 销毁密码明文, 确保安全
+    bzero(pwd_plaintext, sizeof(pwd_plaintext));
+    // 向服务端发送注册信息
+    ret = msg_regist_send(program_stat->connect_fd, &sendbuf);
+    RET_CHECK_BLACKLIST(-1, ret, "msg_regist_send");
+    // 获取服务端的回复信息
+    ret = msg_regist_recv(program_stat->connect_fd, &recvbuf);
+    RET_CHECK_BLACKLIST(-1, ret, "msg_regist_recv");
 
     if (APPROVE == recvbuf.approve) {
         logging(LOG_INFO, "注册成功.");
